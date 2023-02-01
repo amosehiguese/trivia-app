41b40c5a92c5178caf9bd0ab73b64fdb1f9cc5e5
unknown <amosehiguese@gmail.com>
2023-02-01 20:09:33 +0100

    Add difficulty field to Question Model

--- trivia/trivia_app/models.py
+++ trivia/trivia_app/models.py
@@ -6,3 +6,4 @@ class Question(models.Model):
     question = models.TextField()
     answer = models.CharField(max_length=255)
     category = models.ForeignKey('Category', on_delete=models.SET_NULL, related_name='questions')
+    difficulty = models.PositiveIntegerField()
