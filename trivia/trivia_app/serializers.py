bfc5e1850dd50eaa6eec86297a2b43a0ce7e6329
unknown <amosehiguese@gmail.com>
2023-02-02 20:04:33 +0100

    Add CategorySerializer

--- trivia/trivia_app/serializers.py
+++ trivia/trivia_app/serializers.py
@@ -6,3 +6,8 @@ class QuestionSerializer(ModelSerializer):
     class Meta:
         model = Question
         fields = ['id', 'question', 'answer', 'category', 'difficulty']
+
+class CategorySerializer(ModelSerializer):
+    class Meta: 
+        model = Category
+        fields = ['id', 'type']
