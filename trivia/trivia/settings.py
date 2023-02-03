8c554d0dcc19b7559254c5d1382f4376923254d4
unknown <amosehiguese@gmail.com>
2023-02-03 16:33:37 +0100

    Add postgresql database settings to settings.py

--- trivia/trivia/settings.py
+++ trivia/trivia/settings.py
@@ -84,6 +84,14 @@ WSGI_APPLICATION = "trivia.wsgi.application"
 
 DATABASES = {
     "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "triviadb",
+        "USER": os.getenv("USER"),
+        "PASSWORD": os.getenv("PASSWORD"),
+        "HOST": "localhost",
+        "PORT": "5432",
+    },
+    "user1": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
